c2c58e60467dbdbbfe5b70c85d1be9b8a6e36f0f
Lindani SHABANGU <lshabang@c4r15s7.wethinkcode.co.za>
2019-10-16 12:08:01 +0200

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lshabang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/19 21:30:16 by lshabang          #+#    #+#             */
+/*   Updated: 2019/07/31 11:00:19 by lshabang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 3
+# include "libft/libft.h"
+
+int get_next_line(const int fd, char **line);
+
+#endif
